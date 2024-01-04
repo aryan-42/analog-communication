27294020ad2b5e44683bf89191efb2bd7f46089f
aryansingh <aryn.snh@gmail.com>
2024-01-04 23:37:57 +0530

    add PAM & PWM program

---
+++ pulse-width-modulation/pwm.m
@@ -0,0 +1,19 @@
+fc=1000; 
+fs=10000; 
+f1=200; 
+t=0:1/fs:((2/f1)-(1/fs)); 
+x1=0.4*cos(2*pi*f1*t)+0.5; 
+%modulation 
+y1=modulate(x1,fc,fs,'pwm'); 
+subplot(421); 
+plot(x1); 
+title('original signal mesage,f1=500,fs=10000') 
+subplot(422); 
+plot(y1); 
+axis([0 500 -0.2 1.2]); 
+title('PWM') 
+%demodulation 
+x1_recov=demod(y1,fc,fs,'pwm'); 
+subplot(423); 
+plot(x1_recov); 
+title('time domain recovered signal,sigle tone,f1=200');
